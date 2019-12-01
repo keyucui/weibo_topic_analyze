2314483f4cbd0005024caa39d399371c1296b8fc
keyucui <754383904@qq.com>
2019-12-01 14:30:03 +0800

    Create configs.py
    headers cookies 等的配置

---
+++ configs.py
@@ -0,0 +1,29 @@
+Cookie = {
+    # "Cookie":"SINAGLOBAL=611428576138.9926.1529031585340; Ugrow-G0=140ad66ad7317901fc818d7fd7743564; login_sid_t=738f2639b7ec9ed4bda4bda71f80fbe1; cross_origin_proto=SSL; _s_tentry=login.sina.com.cn; Apache=1358246560106.4663.1574947692615; ULV=1574947692624:5:4:2:1358246560106.4663.1574947692615:1574945962066; YF-V5-G0=260e732907e3bd813efaef67866e5183; YF-Page-G0=7f483edf167a381b771295af62b14a27|1574954499|1574954484; crossidccode=CODE-gz-1IAwCP-43YF5X-Pks0uH6BiIchdC4a9cfb2; ALF=1606532846; SSOLoginState=1574996846; SCF=Al_WvdbtXEH8Yao4ordpDA-hA_gh9UtndibTXXHvBmUFdV0yXBqO484CDzp5Bj7z_mKcBav8xgTRM0uEdohuprg.; SUB=_2A25w5Ps_DeRhGeBK6FQU8CvJwjSIHXVTkGv3rDV8PUNbmtBeLUXakW9NR93hbJIseJD1haFYH8STgaMVsRkpM1yY; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W5zny7FLIxKUOfwzh7B98AA5JpX5KzhUgL.FoqXe0qfeh-f1Kn2dJLoIfQLxKqL1K.L1-2LxKBLBonL122LxKML1-BLBK2LxKBLBonL12BLxKqL1hnLBoeLxKML1-2L1hBLxK-L1K5LB-eLxKqLB-BL12eLxK-LBo5L1K2LxK-LBo.LBoBt; SUHB=0FQ7qQX89hc79G; wvr=6; webim_unReadCount=%7B%22time%22%3A1574996883376%2C%22dm_pub_total%22%3A0%2C%22chat_group_client%22%3A0%2C%22allcountNum%22%3A56%2C%22msgbox%22%3A0%7D; UOR=www.google.com.hk,weibo.com,www.google.com"
+    "Cookie": "OUTFOX_SEARCH_USER_ID_NCOO=190685000.04271516; _T_WM=98250732886; MLOGIN=1; ALF=1577588846; SCF=Al_WvdbtXEH8Yao4ordpDA-hA_gh9UtndibTXXHvBmUFmyO4zBrfxMlN7lhYijtYCVMofHDmXW5FC2WkqI33Inc.; SUB=_2A25w5PyKDeRhGeBK6FQU8CvJwjSIHXVQJoTCrDV6PUJbktANLU_TkW1NR93hbEKLhuRhdBRHhHRwXA89Ut8v0cKO; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W5zny7FLIxKUOfwzh7B98AA5JpX5K-hUgL.FoqXe0qfeh-f1Kn2dJLoIfQLxKqL1K.L1-2LxKBLBonL122LxKML1-BLBK2LxKBLBonL12BLxKqL1hnLBoeLxKML1-2L1hBLxK-L1K5LB-eLxKqLB-BL12eLxK-LBo5L1K2LxK-LBo.LBoBt; SUHB=0oU4rKHNUbbvXs; SSOLoginState=1574997210"
+}
+headers = {
+    'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
+    'Host': 'weibo.cn',
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+    }
+
+headers_sub_topic = {
+    'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
+    'Host': 's.weibo.com',
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+}
+
+headers_topic = {
+    'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
+    'Host': 's.weibo.com',
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
+    'Accept-Encoding': 'gzip, deflate, br',
+}
